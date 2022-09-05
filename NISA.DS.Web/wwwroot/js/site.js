84a889cc4007cb35c4c2ba5be681053e7ce82b26
DISHNI <devdishni.freezer@gmail.com>
2022-09-05 17:58:31 +0300

    -  done

--- NISA.DS.Web/wwwroot/js/site.js
+++ NISA.DS.Web/wwwroot/js/site.js
@@ -17,4 +17,43 @@ $(document).ready(function () {
         size: "sm",
         step: 1
     });
+
+    $('.ticket-card-container').slick({
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
 });
